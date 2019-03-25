2a6d99f283e59e12d6f19cdbc416cd1c850ce3dd
Artiom Sitsko <AC3600@yandex.by>
2019-03-25 10:27:49 +0300

    Task 2 added

--- variables/main.js
+++ variables/main.js
@@ -5,3 +5,11 @@ let z = 0;
 let  result = x * y + 5 * z + x - 1;
 
 console.log(`Результат: ${result}`);
+
+//Task 2
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+let average = (algebra + geography + physics) / 3;
+
+console.log(`Среднее арифметическое: ${average.toFixed(2)}`);
