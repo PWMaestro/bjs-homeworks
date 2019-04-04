2b7fffc9b0de24fec028f6674d2e3e274974f317
Artiom Sitsko <AC3600@yandex.by>
2019-04-04 16:09:22 +0300

    Functions Task 3 added

--- functions/main.js
+++ functions/main.js
@@ -79,3 +79,52 @@ console.log(getPersonData({aaa: 1, bbb: 0}));
 
 console.log('Вариант 4 (aaa = 1, bbb = 1):');
 console.log(getPersonData({aaa: 1, bbb: 1}));
+
+//Task 3
+
+let notepad = {
+	algebra: [2, 4, 5, 2, 3, 4],
+	geometry: [2, 4, 5],
+	russian: [3, 3, 4, 5],
+	physics: [5, 5],
+	music: [2, 2, 6],
+	english: [4, 4, 3],
+	poetry: [5, 3, 4],
+	chemistry: [2, 4, 3],
+	french: [4, 4],
+	history: [5, 5, 4, 5],
+	['social science']: [3, 3, 4, 3]
+}
+
+function getAverageScore(data) {
+	let averageScore = {};
+	for (let prop in data) {
+		averageScore[prop] = averageBySubject(data[prop]);
+	}
+	averageScore.average = averageByAllSubjects(averageScore);
+
+	return averageScore;
+}
+
+function averageByAllSubjects(data) {
+	let summ = 0;
+	let numbOfObj = 0;
+	for (let prop in data) {
+		summ += data[prop];
+		numbOfObj++;
+	}
+
+	return summ / numbOfObj;
+}
+
+function averageBySubject(subject) {
+	let summ = 0;
+	for (let i = 0; i < subject.length; i++) {
+		summ += subject[i];
+	}
+
+	return summ / subject.length;
+}
+
+console.log('\n--- Task 3 ---\n\n');
+console.log(getAverageScore(notepad));
