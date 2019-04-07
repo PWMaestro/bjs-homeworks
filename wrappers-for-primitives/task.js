a19889e2772258fc191d057a4157d87f1d83dcbc
Artiom Sitsko <AC3600@yandex.by>
2019-04-07 18:11:31 +0300

    Wrappers for primitives Task 1 added

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,3 +1,5 @@
+'use strict'
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -11,8 +13,45 @@ function calculateMortgage() {
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+	function showWrongValueMessage(name, value) {
+		let phrase = `Параметр "${name}" отсутствует или содержит неправильное значение: ${value}.`;
+		console.log(phrase);
+		return phrase;
+	}
+
+	function checkDateValue(strDate) {
+		let setYear = Number(strDate.substring(0, 4));
+		let setMonth = Number(strDate.substring(5, 7));
+
+		if ((setYear - (new Date).getFullYear()) * 12 + setMonth > ((new Date).getMonth() + 1) ) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	function countMortageTermInMonth(strDate) {
+		return (Number(strDate.substring(0, 4)) - (new Date).getFullYear()) * 12 + ( Number(strDate.substring(5, 7)) - ((new Date).getMonth() + 1) );
+	}
+
+	if (isNaN(parseFloat(percent))) {		
+		return showWrongValueMessage('процентная ставка', percent);
+
+	} else if (isNaN(parseFloat(contribution))) {
+		return showWrongValueMessage('первоначальный взнос', contribution);
+
+	} else if (isNaN(parseFloat(amount))) {
+		return showWrongValueMessage('общая стоимость', amount);
+
+	} else if (checkDateValue(date)) {
+		return showWrongValueMessage('срок ипотеки', date);
+	}
+
+	let term = countMortageTermInMonth(date);
+	let monthPercent = percent / (100 * 12);
+	let creditSum = amount - contribution;
+
+    return ((creditSum * (monthPercent + (monthPercent / (Math.pow((1 + monthPercent), term) - 1) ))) * term).toFixed(2);
 }
 
 function sayHello() {
